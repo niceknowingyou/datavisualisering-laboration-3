cbba7a3218c0f863794b8c5bb75e673c938c8dbd
niceknowingyou <oliver@rmmedia.se>
2026-02-23 15:27:47 +0100

    Added a test-chart and some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Chart as ChartJS } from "chart.js/auto";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
 
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      <div className="dataCard revenueCard">Chart 1
+        <Bar
+          data={{
+            labels: ["A", "B", "C"],
+            datasets: [
+              {
+                label: "Oliver",
+                data: [200,300,400],
+              },
+              {
+                label: "Teodor",
+                data: [90, 80, 70],
+              },
+            ],
+          }}
+        />
+        </div>
+
+
+      <div className="dataCard customerCard">Chart 2</div>
+
+      <div className="dataCard categoryCard">Chart 3</div>
+
+    </div>
+  );
+};
 
 export default App
