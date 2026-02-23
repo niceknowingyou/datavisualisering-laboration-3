4112b00efc2afe2a70792513659a1fd04f1ef501
niceknowingyou <oliver@rmmedia.se>
2026-02-23 17:28:26 +0100

    Added vertical alignment and two more charts

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,55 @@ import "./App.css";
 
 export const App = () => {
   return (
-    <div className="App">
-      <div className="dataCard revenueCard">Chart 1
-        <Bar
-          data={{
-            labels: ["A", "B", "C"],
-            datasets: [
-              {
-                label: "Oliver",
-                data: [200,300,400],
-              },
-              {
-                label: "Teodor",
-                data: [90, 80, 70],
-              },
-            ],
-          }}
-        />
+    <div className="">
+      <div className="App">
+        <div className="dataCard revenueCard">Chart 1
+          <Bar
+            data={{
+              labels: ["A", "B", "C"],
+              datasets: [
+                {
+                  label: "Oliver",
+                  data: [200,300,400],
+                },
+                {
+                  label: "Teodor",
+                  data: [90, 80, 70],
+                },
+              ],
+            }}
+          />
+          </div>
+
+
+        <div className="dataCard customerCard">Uploaded songs to spotify every year.
+          <Line
+            data={{
+              labels: ["2018", "2019", "2020", "2021", "2022", "2023", "2024"],
+              datasets: [
+                {
+                  label: "Uploaded songs",
+                  data: [7e6, 15e6, 22e6, 22e6, 36e6, 40e6, 36e6],
+                },
+              ],
+            }}
+          />
         </div>
 
-
-      <div className="dataCard customerCard">Chart 2</div>
-
-      <div className="dataCard categoryCard">Chart 3</div>
-
+        <div className="dataCard customerCard">Spotify streams for songs uploaded in that year.
+          <Line
+            data={{
+              labels: ["2018", "2019", "2020", "2021", "2022", "2023", "2024"],
+              datasets: [
+                {
+                  label: "Streams",
+                  data: [4.24e9, 5.3e9, 3.67e9, 3.81e9, 4.27e9, 2.8e9, 3.51e9],
+                },
+              ],
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
