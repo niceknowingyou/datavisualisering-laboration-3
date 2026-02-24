2fa60739b7fb877b7addc7f89da96f9f4c72aede
niceknowingyou <oliver@rmmedia.se>
2026-02-24 15:19:37 +0100

    New chart and data

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,38 @@ import Stat from "./stat";
 
 export const App = () => {
   return (
-
     <div className="App">
       <h1>Spotify</h1>
-      <h2>en hemsida av oliver & teodor för <u><a href="https://www.sh.se/program--kurser/program/grund/it-medier-och-design">Södertörns Högskola</a></u></h2>
-      <p>I 2014 så räknades det fram att det fanns ungefär <span className="inText">11 miljoner</span> artister hos Spotify vilket är en ökning på <span className="inText">1.7 miljoner</span> från 2013. Med så här många artister vilka <em>lyssnar</em> faktiskt på musken?</p>
+      <h2>
+        en hemsida av oliver & teodor för{" "}
+        <u>
+          <a href="https://www.sh.se/program--kurser/program/grund/it-medier-och-design">
+            Södertörns Högskola
+          </a>
+        </u>
+      </h2>
+      <p>
+        I 2014 så räknades det fram att det fanns ungefär{" "}
+        <span className="inText">11 miljoner</span> artister hos Spotify vilket
+        är en ökning på <span className="inText">1.7 miljoner</span> från 2013.
+        Med så här många artister vilka <em>lyssnar</em> faktiskt på musken?
+      </p>
+      <Line
+        data={{
+          labels: ["2016", "2018", "2020", "2022", "2023", "2024", "2025"],
+          datasets: [
+            {
+              label: "Monthly Active Users (miljoner)",
+              data: [100, 200, 345, 489, 602, 675, 751],
+            },
+            {
+              label: "Premium (miljoner)",
+              data: [30, 96, 155, 205, 236, 263, 290],
+            },
+          ],
+        }}
+      />
+
       <Bar
         data={{
           labels: ["2024"],
@@ -26,35 +53,36 @@ export const App = () => {
             {
               label: "Artister med mer än 1000 lyssnare per månad",
               data: [742600],
-            }
+            },
           ],
         }}
       />
 
+      <Stat
+        statNumber="Spotify pays $0.003- $0.005 per stream"
+        statText="Det blir ~$4.000 för 1 miljon streams"
+        statColor="red"
+      />
+      <Bar
+        data={{
+          labels: ["2024"],
+          datasets: [
+            {
+              label: "Artister på Spotify",
+              data: [11e7],
+            },
+            {
+              label: "Artister som tjänar mer än 10,000kr per månad",
+              data: [2860000],
+            },
+          ],
+        }}
+        options={{
+          indexAxis: "y",
+        }}
+      />
 
-<Stat statNumber="Spotify pays $0.003- $0.005 per stream" statText="Det blir ~$4.000 för 1 miljon streams" statColor="red"/>
-          <Bar
-            data={{
-              labels: ["2024"],
-              datasets: [
-                {
-                  label: "Artister på Spotify",
-                  data: [11e7],
-                },
-                {
-                  label: "Artister som tjänar mer än 10,000kr per månad",
-                  data: [2860000],
-                },
-              ],
-            }}
-            options={{
-              indexAxis: 'y',
-            }}
-          />
-
-
-
-        <Line
+      <Line
         data={{
           labels: ["2018", "2019", "2020", "2021", "2022", "2023", "2024"],
           datasets: [
@@ -78,8 +106,7 @@ export const App = () => {
         }}
       />
     </div>
-
   );
 };
 
-export default App
+export default App;
